086001949efc472faf8069454ddd0950ad2e774a
G. Ronchi <gronchi@if.usp.br>
2015-12-22 10:45:50 -0200

    Update tests

--- visualization_tools/test_nemax.py
+++ visualization_tools/test_nemax.py
@@ -1,34 +1,44 @@
-"""Test maximum density."""
+# -*- coding: utf-8 -*-
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 sys.path.insert(0, './../src/')
 
 import proc_profile_abel_inversion as pp
 
-fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.25)
-shot = pp.ProcProfile(33708)
-sweeps_average = 33
 
-initial_sweep = shot.time2sweep(32)
-last_sweep = shot.time2sweep(140)
-shot.reference_gd(all_shot=1, sw_clustersize=sweeps_average)
-sweeps = np.arange(initial_sweep, last_sweep, sweeps_average)
-ne_max = np.zeros(len(sweeps))
-t = np.zeros(len(sweeps))
+def main(argv):
+    """Test maximum density."""
+    if len(argv) < 1:
+        shot = 33772
+    else:
+        shot = int(argv[1])
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(bottom=0.25)
+    shot = pp.ProcProfile(shot)
+    sweeps_average = 33
 
-for i in range(len(sweeps)):
-    print("Sweep: %d" % sweeps[i])
-    shot.plasma_gd(sweeps[i], sweeps_average, all_shot=1)
-    shot.find_ne_max2()
-    ne_max[i] = shot.ne_max
-    t[i] = shot.sweep2time(sweeps[i])
+    initial_sweep = shot.time2sweep(32)
+    last_sweep = shot.time2sweep(140)
+    shot.reference_gd(all_shot=1, sw_clustersize=sweeps_average)
+    sweeps = np.arange(initial_sweep, last_sweep, sweeps_average)
+    ne_max = np.zeros(len(sweeps))
+    t = np.zeros(len(sweeps))
 
-plt.plot(t, ne_max*1e-19)
-plt.ylabel("ne [$10^{19}$ m$^-3$] ")
-plt.xlabel("time (ms)")
-plt.title("# %s" % (shot.shot))
+    for i in range(len(sweeps)):
+        print("Sweep: %d" % sweeps[i])
+        shot.plasma_gd(sweeps[i], sweeps_average, all_shot=1)
+        shot.find_ne_max2()
+        ne_max[i] = shot.ne_max
+        t[i] = shot.sweep2time(sweeps[i])
 
-plt.show()
+    plt.plot(t, ne_max * 1e-19)
+    plt.ylabel("ne [$10^{19}$ m$^-3$] ")
+    plt.xlabel("time (ms)")
+    plt.title("# %s" % (shot.shot))
+
+    plt.show()
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
--- visualization_tools/test_profile_bottollier2.py
+++ visualization_tools/test_profile_bottollier2.py
@@ -1,20 +1,22 @@
+# -*- coding: utf-8 -*-
+
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import integrate, optimize, special
+from scipy import integrate, optimize, interpolate
 import sys
 sys.path.insert(0, './../src/')
 
 import proc_profile_bottollier as BC
 
 
-def ne(r, n0=1.65e19, alpha=1.2):
+def density(r, n0=1.65e19, alpha=1.2):
     """Perfil radial de densidade."""
     return n0 * np.power((1 - (r / .18) ** 2), alpha)
 
 
 def r2f_plasma(r, n0=1.65e19, alpha=1.2):
     """Perfil radial da frequência de plasma."""
-    return 8.978663 * np.sqrt(ne(r, n0, alpha))
+    return 8.978663 * np.sqrt(density(r, n0, alpha))
 
 
 def f_plasma2ne(f):
@@ -30,12 +32,12 @@ def f_plasma2rc(f, n0=1.65e19, alpha=1.2):
 #    Solução genérica para qualquer formato de densidade
 #    (desabilitado porque nem sempre consegue convergir)
     if np.size(f) == 1:
-        fun = lambda r: 1e-9 * ne(r, n0, alpha) - 1e-9 * f_plasma2ne(f)
+        fun = lambda r: 1e-9 * density(r, n0, alpha) - 1e-9 * f_plasma2ne(f)
         rc = optimize.fsolve(fun, 0.12)
     else:
         rc = np.zeros(np.size(f))
         for i in range(np.size(rc)):
-            fun = lambda r: 1e-9 * ne(r, n0, alpha) - 1e-9 * f_plasma2ne(f[i])
+            fun = lambda r: 1e-9 * density(r, n0, alpha) - 1e-9 * f_plasma2ne(f[i])
             rc[i] = optimize.fsolve(fun, 0.12)
     return rc
 
@@ -48,7 +50,7 @@ def n_index(r, f,  n0=1.65e19, alpha=1.2):
 def phase_shift(fc, n0=1.65e19, alpha=1.2):
     phi = np.zeros(len(fc))
     for i in range(len(phi)):
-        phi[i] = (4. * np.pi * fc[i] / 3e8) * integrate.quad(n_index, f_plasma2rc(fc[i]), 0.18, args=(fc[i],), epsabs=1e-14)[0] - np.pi / 2
+        phi[i] = (4. * np.pi * fc[i] / 3e8) * integrate.quad(n_index, f_plasma2rc(fc[i], n0, alpha), 0.18, args=(fc[i], n0, alpha,), epsabs=1e-14)[0] - np.pi / 2
     return phi
 
 
@@ -69,29 +71,35 @@ def group_delay(f_probe, n0=1.65e19, alpha=1.2):
 if __name__ == "__main__":
     n0 = 1.65e19
     alpha = 1.2
-    f_probe = np.linspace(16e9, np.min([35e9, r2f_plasma(0.001)]), 500)  # frequência de sondagem experimental
+    f_probe = np.linspace(16e9, np.min([35e9, r2f_plasma(0.001)]), 100)  # frequência de sondagem experimental
     tau = group_delay(f_probe, n0, alpha)
     phi = phase_shift(f_probe, n0, alpha)
     # preciso chamar a classe, mas só uso os dados simulados
     shot = BC.Bottollier(32111)
-    # fig = plt.figure()
-    # plt.plot(f_probe * 1e-9, phi)
-    # plt.ylabel('phi [rad]')
-    # plt.xlabel('probing frequency [GHz]')
-    # plt.show()
 
-    # plt.plot(f_probe * 1e-9, tau * 1e9)
-    # plt.ylabel('group time delay [ns]')
-    # plt.xlabel('probing frequency [GHz]')
-    # plt.show()
+    # inicializacao linear
+    # f_probe = np.append(np.linspace(1e9, f_probe[0], num=16, endpoint=False), f_probe)
+    # phi = np.append(np.linspace(-np.pi/2, phi[0], num=16, endpoint=False), phi)
 
-    r_real = np.linspace(0, 0.18, 100)
-    dens = ne(r_real, n0, alpha)
+    # inicializacao cubica
+    # phi = np.append(np.polyval([(phi[0] + np.pi) / f_probe[1] ** 3., 0, 0, -np.pi/2], np.linspace(1e9, f_probe[0], num=10, endpoint=False)), phi)
+    # f_probe = np.append(np.linspace(1e9, f_probe[0], num=10, endpoint=False), f_probe)
+    phi = np.append(interpolate.interp1d([0, 1e9, f_probe[0], f_probe[5]], [-np.pi/2, -0.999 * np.pi/2, phi[0], phi[5]], kind='cubic')(np.linspace(1e9, f_probe[0], num=20, endpoint=False)), phi)
+    f_probe = np.append(np.linspace(1e9, f_probe[0], num=20, endpoint=False), f_probe)
+
+    plt.plot(f_probe * 1e-9, phase_shift(f_probe, n0, alpha), 'r--')
+    plt.plot(f_probe * 1e-9, phi, '.')
+    plt.ylabel('phase [rad]')
+    plt.xlabel('probing frequency [GHz]')
+    plt.show()
+
+    r_real = np.linspace(0, 0.18, 50)
+    ne = density(r_real, n0, alpha)
     rc_BC = shot.find_pos(f_probe * 1e-9, phi)
+    ne = f_plasma2ne(f_probe)
     fig = plt.figure()
-    # tive que multiplicar por esse fator 0.02 para obter algo razoavel
-    plt.plot(rc_BC * 0.02, f_plasma2ne(f_probe) * 1e-19, 'b-', label='BC')
-    plt.plot(r_real[::-1], dens * 1e-19, 'r--', label='real')
+    plt.plot(rc_BC, ne * 1e-19, 'b.', label='BC')
+    plt.plot(r_real[::-1], density(r_real, n0, alpha) * 1e-19, 'r--', label='real')
     plt.ylabel('ne [10^19 /m^3]')
     plt.xlabel('posicao radial [m]')
     plt.xlim([0, 0.18])
